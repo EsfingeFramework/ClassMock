73741cd527eab78f2899f4c037dd8b75de5e5d87
Marco Nardes <marconardes@gmail.com>
2016-08-23 12:15:51 -0300

    Update classpath

---
+++ ClassMock/src/test/java/net/sf/esfinge/classmock/example/method/AuthorizationProxyTest.java
@@ -0,0 +1,55 @@
+package net.sf.esfinge.classmock.example.method;
+
+import static net.sf.esfinge.classmock.ClassMockUtils.invoke;
+
+import org.jmock.Expectations;
+import org.jmock.Mockery;
+import org.jmock.integration.junit4.JMock;
+import org.jmock.integration.junit4.JUnit4Mockery;
+import org.jmock.lib.legacy.ClassImposteriser;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import net.sf.esfinge.classmock.ClassMock;
+
+@RunWith(JMock.class)
+public class AuthorizationProxyTest {
+	
+	Mockery context = new JUnit4Mockery(){{
+        setImposteriser(ClassImposteriser.INSTANCE);
+    }};
+    
+    private Object mock;
+    
+    @Before
+    public void createMock(){
+    	ClassMock classMock  = new ClassMock("DummyInterface",true);
+    	classMock.addAbstractMethod(void.class,"execute")
+    		.addMethodAnnotation("execute", AuthorizedRoles.class,new String[]{"admin"});
+    	Class interf = classMock.createClass();
+    	
+		mock = context.mock(interf);
+    }
+	
+    @Test
+	public void authorizedMethod() throws Throwable {
+		Object proxy = AuthorizationProxy.createProxy(mock, new User("john","admin"));
+		context.checking(new Expectations() {{
+        	invoke(one (mock),"execute");
+        }});
+		invoke(proxy,"execute");
+	}
+    
+    @Test(expected=AuthorizationException.class)
+    public void unauthorizedMethod() throws Throwable {
+		Object proxy = AuthorizationProxy.createProxy(mock, new User("john","operator"));
+		
+		context.checking(new Expectations() {{
+        	invoke(never (mock),"execute");
+        }});
+		
+		invoke(proxy,"execute");
+	}
+
+}
