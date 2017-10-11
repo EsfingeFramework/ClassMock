db085b3bdcf99c0dbd1c5317d5a69437fcc1be32
Marcos Nazário <carvalhonazario@gmail.com>
2017-10-11 10:03:20 -0300

    removed.

--- ClassMock/src/main/java/net/sf/esfinge/classmock/Location.java
+++
@@ -1,7 +0,0 @@
-package net.sf.esfinge.classmock;
-
-public enum Location {
-	GETTER,
-	SETTER,
-	FIELD;
-}
--- ClassMock/src/test/java/Dummy.java
+++
@@ -1,9 +0,0 @@
-import net.sf.esfinge.classmock.ClassMock;
-import net.sf.esfinge.classmock.example.basic.Teste;
-
-
-public class Dummy {
-	@Teste(classe=ClassMock.class)
-	public void metodo(){}
-
-}
