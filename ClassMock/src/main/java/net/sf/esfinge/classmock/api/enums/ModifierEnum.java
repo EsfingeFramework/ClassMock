011fe362a9b3f772893c5f7e62402fb6903e09ad
Marcos Nazário <carvalhonazario@gmail.com>
2017-10-16 13:59:37 -0300

    add ANNOTATION modifier and better javadoc

--- ClassMock/src/main/java/net/sf/esfinge/classmock/api/enums/ModifierEnum.java
+++ ClassMock/src/main/java/net/sf/esfinge/classmock/api/enums/ModifierEnum.java
@@ -4,49 +4,52 @@ import org.objectweb.asm.Opcodes;
 
 public enum ModifierEnum {
 
-    /** ENUM */
+    /** ANNOTATION to class */
+    ANNOTATION(Opcodes.ACC_ANNOTATION), // class
+
+    /** ENUM to class(?) field inner */
     ENUM(Opcodes.ACC_ENUM), // class(?) field inner
 
-    /** SUPER */
+    /** SUPER to class */
     SUPER(Opcodes.ACC_SUPER), // class
 
-    /** INTERFACE */
+    /** INTERFACE to class */
     INTERFACE(Opcodes.ACC_INTERFACE), // class
 
-    /** ABSTRACT */
+    /** ABSTRACT to class and method */
     ABSTRACT(Opcodes.ACC_ABSTRACT), // class, method
 
-    /** STATIC */
+    /** STATIC to field and method */
     STATIC(Opcodes.ACC_STATIC), // field, method
 
-    /** FINAL */
+    /** FINAL to class, field, method andparameter */
     FINAL(Opcodes.ACC_FINAL), // class, field, method, parameter
 
-    /** SYNCHRONIZED */
+    /** SYNCHRONIZED to method */
     SYNCHRONIZED(Opcodes.ACC_SYNCHRONIZED), // method
 
-    /** VOLATILE */
+    /** VOLATILE to field */
     VOLATILE(Opcodes.ACC_VOLATILE), // field
 
-    /** BRIDGE */
+    /** BRIDGE to method */
     BRIDGE(Opcodes.ACC_BRIDGE), // method
 
-    /** VARARGS */
+    /** VARARGS to method */
     VARARGS(Opcodes.ACC_VARARGS), // method
 
-    /** TRANSIENT */
+    /** TRANSIENT to field */
     TRANSIENT(Opcodes.ACC_TRANSIENT), // field
 
-    /** NATIVE */
+    /** NATIVE to method */
     NATIVE(Opcodes.ACC_NATIVE), // method
 
-    /** STRICT */
+    /** STRICT to method */
     STRICT(Opcodes.ACC_STRICT), // method
 
-    /** SYNTHETIC */
+    /** SYNTHETIC to class, field, method and parameter */
     SYNTHETIC(Opcodes.ACC_SYNTHETIC), // class, field, method, parameter
 
-    /** MANDATED */
+    /** MANDATED to parameter */
     MANDATED(Opcodes.ACC_MANDATED); // parameter
 
     private int opCodes;
